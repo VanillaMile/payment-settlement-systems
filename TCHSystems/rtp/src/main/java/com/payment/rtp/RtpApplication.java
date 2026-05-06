3d92900a19bab88d79f3dc2784629eefb6229094
Krzysztof Hurniak <krzysh32@interia.pl>
2026-05-06 03:34:46 +0200

    rtp init

---
+++ TCHSystems/rtp/src/main/java/com/payment/rtp/RtpApplication.java
@@ -0,0 +1,13 @@
+package com.payment.rtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtpApplication.class, args);
+	}
+
+}
